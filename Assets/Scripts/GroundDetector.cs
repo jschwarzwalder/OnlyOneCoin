60c3ad54f7830044374580a0159721918b4dca2a
jschwarzwalder <jamischwarzwader@gmail.com>
2019-08-02 22:51:25 -0700

    Added simple player movement script

---
+++ Assets/Scripts/GroundDetector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundDetector : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger");
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("Ground");
+            SendMessageUpwards("AddGround");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            SendMessageUpwards("RemoveGround");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MovementSpeed;
+    public float JumpSpeed;
+    public bool OnGround { get { return groundCount > 0; } }
+
+    private new Rigidbody rigidbody;
+    private int groundCount;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float movementDelta = MovementSpeed * Time.deltaTime * horizontal;
+        rigidbody.MovePosition(transform.position + transform.right * movementDelta);
+
+        bool jump = Input.GetButtonDown("Jump");
+        if (jump && OnGround)
+        {
+            Debug.Log("Jump");
+            rigidbody.velocity += transform.up * JumpSpeed;
+        }
+    }
+
+    public void AddGround()
+    {
+        Debug.Log("Received");
+        ++groundCount;
+    }
+
+    public void RemoveGround()
+    {
+        --groundCount;
+    }
+}

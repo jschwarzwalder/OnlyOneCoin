59668639a620e220654ba022694a2cbcb9388682
jschwarzwalder <jamischwarzwader@gmail.com>
2019-08-03 00:48:26 -0700

    Functional 3D Movement

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,28 +6,66 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform Target;
     public float FollowDistance;
-    public float MaxSpeed = 10;
-    public float MaxAngularSpeed = 20;
+    public float MouseSensitivity = 5;
+    public float SmoothTime = 0.3f;
+    
+    [SerializeField]
+    private float minAngleX = 0;
+    [SerializeField]
+    private float maxAngleX = 60;
 
-    public float MinAngleY = -180;
-    public float MaxAngleY =  180;
-    public float MinAngleX = -60;
-    public float MaxAngleX =  60;
+    private float minAngleXRad;
+    private float maxAngleXRad;
 
     private float angleY;
     private float angleX;
+
+    private float angleYVelocity;
+    private float angleXVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
         Vector3 eulerAngles = transform.rotation.eulerAngles;
-        angleY = eulerAngles.y;
-        angleX = eulerAngles.x;
+        angleY = Mathf.Deg2Rad * eulerAngles.y;
+        angleX = Mathf.Deg2Rad * eulerAngles.x;
+
+        Debug.Log(eulerAngles);
+
+        minAngleXRad = Mathf.Deg2Rad * minAngleX;
+        maxAngleXRad = Mathf.Deg2Rad * maxAngleX;
+
+        updatePosition();
+    }
+
+    private void updatePosition()
+    {
+
+        float offsetY = FollowDistance * Mathf.Sin(angleX);
+        Vector3 offsetOnXZPlane = new Vector3(-Mathf.Sin(angleY), 0, -Mathf.Cos(angleY));
+        offsetOnXZPlane *= (FollowDistance * Mathf.Cos(angleX));
+
+        Vector3 offset = new Vector3(offsetOnXZPlane.x, offsetY, offsetOnXZPlane.z);
+        transform.position = Target.position + offset;
+        transform.LookAt(Target);
+
+        Debug.Log("offset: " + offset);
+        Debug.Log("Distance: " + offset.magnitude);
     }
 
     // Update is called once per frame
     void Update()
     {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
 
+        float targetAngleY = angleY + mouseX * MouseSensitivity;
+        float targetAngleX = angleX + mouseY * MouseSensitivity;
+        targetAngleX = Mathf.Clamp(targetAngleX, minAngleXRad, maxAngleXRad);
+
+        angleY = Mathf.SmoothDampAngle(angleY, targetAngleY, ref angleYVelocity, SmoothTime);
+        angleX = Mathf.SmoothDampAngle(angleX, targetAngleX, ref angleXVelocity, SmoothTime);
+
+        updatePosition();
     }
 }

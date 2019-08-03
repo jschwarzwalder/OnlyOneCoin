034927c9486d05ae69862eded6afe92f17fda40e
jschwarzwalder <jamischwarzwader@gmail.com>
2019-08-03 11:21:04 -0700

    Added ability to collect coins

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public static HashSet<string> AllDescriptors = new HashSet<string>();
+
+    public string[] Descriptors;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (string descriptor in Descriptors)
+        {
+            AllDescriptors.Add(descriptor);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CoinCollector.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollector : MonoBehaviour
+{
+    private HashSet<string> collectedCoins = new HashSet<string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Coin coin = other.GetComponent<Coin>();
+        if (coin != null)
+        {
+            foreach (string descriptor in coin.Descriptors)
+            {
+                if (collectedCoins.Contains(descriptor))
+                {
+                    Debug.Log("You Lose");
+                }
+                else
+                {
+                    collectedCoins.Add(descriptor);
+                }
+            }
+
+            if (collectedCoins.Count == Coin.AllDescriptors.Count)
+            {
+                Debug.Log("You Win");
+            }
+
+            Destroy(coin.gameObject);
+        }
+    }
+}

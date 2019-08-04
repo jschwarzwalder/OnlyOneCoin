e9c268f5e303a5714506a024bc6e50528f1e5fe2
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2019-08-04 01:24:37 -0700

    repushing last level

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -11,7 +11,6 @@ public class SceneController : MonoBehaviour
     [SerializeField] private AudioSource StartLevelSound;
 
     public float TimeLeft { get { return nextTime - Time.time; } }
-    public int CurrentLevel { get { return sceneIndex + 1; } }
 
     private int sceneIndex = 0;
     private bool sceneLoaded = false;

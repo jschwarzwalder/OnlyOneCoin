a560525974d3cef10c871fcd663a204d6d113688
jschwarzwalder <jamischwarzwader@gmail.com>
2019-08-04 01:08:32 -0700

    Added "Get Ready" text, and Level Indicator

---
+++ Assets/Scripts/LevelLabel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLabel : MonoBehaviour
+{
+    private Text text;
+    private SceneController sceneController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoad;
+        SceneManager.sceneUnloaded += OnSceneUnload;
+        text = GetComponent<Text>();
+        GameObject gameManager = GameObject.FindGameObjectWithTag("GameController");
+        sceneController = gameManager.GetComponent<SceneController>();
+    }
+
+    private void OnSceneUnload(Scene arg0)
+    {
+        text.text = "Level " + sceneController.CurrentLevel;
+    }
+
+    private void OnSceneLoad(Scene arg0, LoadSceneMode arg1)
+    {
+        text.text = "Level " + sceneController.CurrentLevel;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/TimerLabel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TimerLabel : MonoBehaviour
+{
+    private Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoad;
+        SceneManager.sceneUnloaded += OnSceneUnload;
+        text = GetComponent<Text>();
+    }
+
+    private void OnSceneUnload(Scene arg0)
+    {
+        text.text = "Get Ready";
+    }
+
+    private void OnSceneLoad(Scene arg0, LoadSceneMode arg1)
+    {
+        text.text = "Time Left";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
